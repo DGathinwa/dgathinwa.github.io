934a65cd95da25e6a662edc4849f4fb3fb39c052
Daniel Gathinwa <dgathinwa@gmail.com>
2025-07-07 22:30:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 // Replace the below values with your actual EmailJS keys
 
 (function(){
-  emailjs.init(hRWqjVvUufsLDea_g); // Replace this with your public key
+  emailjs.init("hRWqjVvUufsLDea_g"); // Replace this with your public key
 })();
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
   event.preventDefault();
   const status = document.getElementById("form-status");
 
-  emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this)
+  emailjs.sendForm("service_575c7rk", 'template_3ksib4d', this)
     .then(() => {
       status.textContent = "Message sent successfully! ✅";
       this.reset();
